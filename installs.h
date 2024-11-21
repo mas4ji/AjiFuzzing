1daf428818ac72518f08d9f7dd21d4b4c03ea932
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-11-21 13:32:08 +0700

    Create installs.h

---
+++ installs.h
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Menyalin dan memindahkan file AjiFuzzer.sh ke /usr/bin/aji
+sudo cp AjiFuzzer.sh /usr/bin/aji
+
+# Memberikan izin eksekusi pada file AjiFuzzer
+sudo chmod u+x /usr/bin/aji
+
+echo "AjiFuzzer telah berhasil diinstal! Sekarang masukkan perintah 'aji' untuk menjalankan alat ini."
