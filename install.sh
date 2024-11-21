05accd58736f1da5d76c50e652ed02a221679a93
Mas Aji <127058700+mas4ji@users.noreply.github.com>
2024-11-21 14:39:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 # Menyalin dan memindahkan file AjiFuzzer.sh ke /usr/bin/aji
 if [ -f "AjiFuzzer.sh" ]; then
-    sudo cp AjiFuzzer.sh /usr/bin/aji
+    sudo cp AjiFuzzing.sh /usr/bin/aji
 else
-    echo "File AjiFuzzer.sh tidak ditemukan!"
+    echo "File AjiFuzzing.sh tidak ditemukan!"
     exit 1
 fi
 
 # Memberikan izin eksekusi pada file AjiFuzzer
 sudo chmod u+x /usr/bin/aji
 
-echo "AjiFuzzer telah berhasil diinstal! Sekarang masukkan perintah 'aji' untuk menjalankan alat ini."
+echo "AjiFuzzing telah berhasil diinstal! Sekarang masukkan perintah 'aji' untuk menjalankan alat ini."
